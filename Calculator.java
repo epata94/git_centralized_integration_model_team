ff336953241af3e7564e98c2434a8bcb13c324e7
epata94 <hklee0724@gmail.com>
2025-07-24 15:19:05 +0900

    개발 시작

--- Calculator.java
+++ Calculator.java
@@ -1,3 +1,13 @@
 public class Calculator{
 	
+	public static void main(String[] args){
+		Scanner scanner = new Scanner(System.in);
+
+        System.out.println("간단한 콘솔 계산기 프로그램");
+        System.out.print("첫 번째 숫자를 입력하세요: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("두 번째 숫자를 입력하세요: ");
+        double num2 = scanner.nextDouble();
+	}
 }
